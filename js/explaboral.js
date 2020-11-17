694d660065c53a374e1c99c69b72be9a81ecd7c0
pminkevich <pminkevich@gmail.com>
2020-11-17 17:17:13 -0300

    New Version

--- js/explaboral.js
+++ js/explaboral.js
@@ -1,35 +1,35 @@
-Vue.component('exp-laboral',{
+Vue.component('exp-laboral', {
 
- data: function(){
+    data: function () {
 
-    return {
-        explaboral:{
-            titulo:'Experiencia Laboral',
-            trabajos:[{
-                    titulo:'MERCEDES BENZ: ',
-                    anio:'(2005-2018)',
-                    contenido:'Areas de Montaje, Pintura y Mantenimiento.'
-                    },
-                    {
-                    titulo:'WEBMASTER SITIOSWEBGRATIS:',
-                    anio:'(2008 - 2012)',
-                    contenido:'Utilizando PHP y MSQL, Con Servidor Propio Bajo Sistema Operativo Linux, Todos los Sitios posicionados en Google.'
-                
-                    },
-                    {
-                    titulo:'DESARROLLADOR:',
-                    anio:'(2000-2020)',
-                    lista:[{ejemplo:'Sistema de Gestión CallCenter (Desktop App + Web Api + PWA)'},
-                    {ejemplo2:'Control de Historias Clínicas (Servicio de Ortopedia Y Traumatología, Hospital Paroissien)'},
-                    {ejemplo3:'Sistema Control de Stock y Venta Mostrador'}, 
-                    {ejemplo4:'Sistema de Gestión Taller (Para Ler Sistemas & Servicios)'}
+        return {
+            explaboral: {
+                titulo: 'Experiencia Laboral',
+                trabajos: [{
+                    titulo: 'MERCEDES BENZ: ',
+                    anio: '(2005-2018)',
+                    contenido: 'Areas de Montaje, Pintura y Mantenimiento.'
+                },
+                {
+                    titulo: 'WEBMASTER SITIOSWEBGRATIS:',
+                    anio: '(2008 - 2012)',
+                    contenido: 'Utilizando PHP y MSQL, Con Servidor Propio Bajo Sistema Operativo Linux, Todos los Sitios posicionados en Google.'
+
+                },
+                {
+                    titulo: 'DESARROLLOS:',
+                    anio: '(2020)',
+                    lista: [{ ejemplo: 'Sistema de Gestión CallCenter (Desktop App + Web Api + PWA)' },
+                    { ejemplo2: 'App Web Estudio de Abogadas (En Desarrollo)' },
+                    { ejemplo3: 'Proyecto Colaborativo Empresa de Transporte Colombia' },
+                    { ejemplo4: 'Nuevo sitio de indumov.com (En Etapa de Diseño)' }
                     ]
-                    }]
-        }
+                }]
+            }
 
-    }
- },
- template:`<div class="whitebox">
+        }
+    },
+    template: `<div class="whitebox">
  <h4 class="titulo">{{explaboral.titulo}} </h4>
   <h5 class="subtitulo"><strong>{{explaboral.trabajos[0].titulo}}</strong></h5>
   <i class="itemsleft">{{explaboral.trabajos[0].anio}}</i>
@@ -44,10 +44,25 @@ Vue.component('exp-laboral',{
   <h5 class="subtitulo"><strong>{{explaboral.trabajos[2].titulo}}</strong></h5>
   <i class="itemsleft">{{explaboral.trabajos[2].anio}}</i>
      <ul>
-         <li class="itemsleft">{{explaboral.trabajos[2].lista[0].ejemplo}}</li>
-         <li class="itemsleft">{{explaboral.trabajos[2].lista[1].ejemplo2}}</li>
-         <li class="itemsleft">{{explaboral.trabajos[2].lista[2].ejemplo3}}</li>
-         <li class="itemsleft">{{explaboral.trabajos[2].lista[3].ejemplo4}}</li>
+         <li class="itemsleft"><strong>{{explaboral.trabajos[2].lista[0].ejemplo}}</strong> 
+         <br>
+         <a href="https://youtu.be/UsI5Zclac50" target="_blank">Ver Video Proyecto Completo</a>
+         <br>
+         <a href="https://callcenter-81703.web.app/login">Link de la PWA</a>
+         <br>
+         <a href="https://mycallapp-6fb21.web.app/home">Link de la Web App</a>
+         <br>
+          <a href="https://github.com/pminkevich/Repos/tree/master/CallcenterAPI">Api en Net Core</a>
+         </li>
+         <li class="itemsleft"><strong>{{explaboral.trabajos[2].lista[1].ejemplo2}}</strong>
+         <br>
+        <a href="https://abogadas-eff0e.web.app/home">Link del Sitio</a>
+         </li>
+         <li class="itemsleft"><strong>{{explaboral.trabajos[2].lista[2].ejemplo3}}</strong>
+         <br>
+        <a href="https://proyecto-ed3ff.web.app/login">Link del Sitio</a>
+         </li>
+         <li class="itemsleft"><strong>{{explaboral.trabajos[2].lista[3].ejemplo4}}</strong></li>
      </ul>
      <div  class="links arrow">
     <div><router-link  class="links rightArrow" to="/Education">
--- js/timeline.js
+++ js/timeline.js
@@ -1,7 +1,7 @@
 
-Vue.component('timeline',{
+Vue.component('timeline', {
 
-template:`<div class="timeline" >
+    template: `<div class="timeline" >
 <br>
 <div  v-if="videoEnable" class="videoBox" ><div  class="closeButton btn btn-dark btn-sm float-right">x</div><div v-html="videoContent"></div></div>
    <div  v-if="windowEnable" class="windowBox" ><div v-on:click="windowClose()" class="closeButton btn btn-dark btn-sm float-right">x</div><div v-html="windowContent"></div></div>
@@ -19,68 +19,68 @@ template:`<div class="timeline" >
 <br>
 
 </div>`,
-// props:{windowContent:String},
-data: function(){
-    return {
-        windowContent:'',
-        videoContent:'<p>content</p',
-        videoEnable:false,
-        windowEnable:false,
-        timeline:[
-            // {
-            // tittle:'Inicio', 
-            // content:'Comencé a programar en Pascal en una XT-286'
-            // },
-            {
-            tittle:'2000', 
-            content:'Desarrolle una Aplicación para el Servicio de Ortopedia y Traumatología del Hospital Paroissiene'
-            },
-            {
-                tittle:'2001', 
-                content:'Comienzo a desarrollar sitios web con ASP'
-            },
-            {
-                tittle:'2004', 
-                content:'Apertura de Local de Computación'
-            },
-            {
-                tittle:'2005', 
-                content:'Entro a Mercedes Benz'
-            },
-            {
-                tittle:'2008', 
-                content:'Nace SitiosWebGratis'
-            },
-            {
-                tittle:'2020', 
-                content:'Proyecto Callcenter <br><a href="https://youtu.be/cJgs_-9NSEA" target="_blank">Ver Video</a>'
-            }
-        ],
-    }
-},
-methods:{
-    windowOpen: function(index){
-        this.windowEnable=true;
-        this.windowContent=this.timeline[index].content;
-         
-        },
-        windowClose: function(){
-            
+    // props:{windowContent:String},
+    data: function () {
+        return {
+            windowContent: '',
+            videoContent: '<p>content</p',
+            videoEnable: false,
+            windowEnable: false,
+            timeline: [
+                // {
+                // tittle:'Inicio', 
+                // content:'Comencé a programar en Pascal en una XT-286'
+                // },
+                {
+                    tittle: '2000',
+                    content: 'Desarrolle una Aplicación para el Servicio de Ortopedia y Traumatología del Hospital Paroissiene'
+                },
+                {
+                    tittle: '2001',
+                    content: 'Comienzo a desarrollar sitios web con ASP'
+                },
+                {
+                    tittle: '2004',
+                    content: 'Apertura de Local de Computación'
+                },
+                {
+                    tittle: '2005',
+                    content: 'Entro a Mercedes Benz'
+                },
+                {
+                    tittle: '2008',
+                    content: 'Nace SitiosWebGratis'
+                },
+                {
+                    tittle: '2020',
+                    content: 'Proyecto Callcenter <br><a href="https://youtu.be/UsI5Zclac50" target="_blank">Ver Video</a>'
+                }
+            ],
+        }
+    },
+    methods: {
+        windowOpen: function (index) {
+            this.windowEnable = true;
+            this.windowContent = this.timeline[index].content;
 
-         
-                 this.windowEnable=false;
-                
-             }
+        },
+        windowClose: function () {
+
+
+
+            this.windowEnable = false;
+
+        }
         // videoOpen: function(){
-          
+
         //     this.videoEnable=true;
-          
+
         // }
     },
-   
-        
-        
-    }
-    
+
+
+
+}
+
 
 )
